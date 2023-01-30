33d6f902dcd1cb70c616cf7ecd0609fc93bb385f
Jelby <evgeniy.ipatov92@mail.ru>
2023-01-30 18:13:48 +0300

    refactor: (backend). moved db variables to .env file

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '../.env',
+    }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'project_requests',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       models: [],
       autoLoadModels: true,
     }),
